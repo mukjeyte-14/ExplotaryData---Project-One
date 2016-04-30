aae3100196f7c6f2313d5f9dd75d33d8c9189929
mukjeyte-14 <mukjeyte@msn.com>
2016-04-30 16:29:43 -0400

    Added files via upload

---
+++ plot2.R
@@ -0,0 +1,20 @@
+## Getting full dataset
+full_data <- read.csv("~/DataScience/ExplotaryData/Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+full_data$Date <- as.Date(full_data$Date, format="%d/%m/%Y")
+
+## Subsetting the data
+data <- subset(full_data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+## Little House Cleaning
+rm(full_data)
+
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 2
+plot(data$Global_active_power~data$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
